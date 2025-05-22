85605e47dff8ac0ce9cea473b8edf9717687da17
Charlie Norton <22cnorton@gmail.com>
2025-05-22 12:24:08 -0400

    creats Focus enum to replace foucs: u8

--- src/components/page_language_popup.rs
+++ src/components/page_language_popup.rs
@@ -16,14 +16,11 @@ use crate::{
     ui::{centered_rect, StatefulList},
 };
 
-use super::Component;
-
-const FOCUS_INPUT: u8 = 0;
-const FOCUS_LIST: u8 = 1;
+use super::{Component, Focus};
 
 pub struct PageLanguageSelectionComponent {
     input: Input,
-    focus: u8,
+    focus: Focus,
     list: StatefulList<LanguageLink>,
     language_links: Vec<LanguageLink>,
 
@@ -37,7 +34,7 @@ impl PageLanguageSelectionComponent {
             input: Input::default(),
             list: StatefulList::with_items(language_links.clone()),
             language_links,
-            focus: 0,
+            focus: Focus::default(),
 
             config,
             theme,
@@ -94,21 +91,20 @@ impl Component for PageLanguageSelectionComponent {
 
         match key.code {
             KeyCode::Tab | KeyCode::BackTab => {
-                if self.focus == FOCUS_INPUT {
-                    self.focus = FOCUS_LIST;
-                } else if self.focus == FOCUS_LIST {
-                    self.focus = FOCUS_INPUT;
-                }
+                self.focus = match self.focus {
+                    Focus::Input => Focus::List,
+                    Focus::List => Focus::Input,
+                };
 
-                tracing::debug!("focus now: '{}'", self.focus);
+                tracing::debug!("focus now: '{:?}'", self.focus);
 
                 ActionResult::consumed()
             }
-            KeyCode::Char('i') if self.focus == FOCUS_LIST => {
-                self.focus = FOCUS_INPUT;
+            KeyCode::Char('i') if self.focus == Focus::List => {
+                self.focus = Focus::Input;
                 ActionResult::consumed()
             }
-            _ if self.focus == FOCUS_INPUT => {
+            _ if self.focus == Focus::Input => {
                 self.input.handle_event(&crossterm::event::Event::Key(key));
                 self.update_list();
                 ActionResult::consumed()
@@ -172,7 +168,7 @@ impl Component for PageLanguageSelectionComponent {
             .scroll((0, scroll as u16));
         f.render_widget(input_widget, input_area);
 
-        if self.focus == FOCUS_INPUT {
+        if self.focus == Focus::Input {
             f.set_cursor(
                 input_area.x + (cursor.max(scroll) - scroll) as u16,
                 input_area.y,
@@ -184,7 +180,7 @@ impl Component for PageLanguageSelectionComponent {
             .get_items()
             .iter()
             .map(|x| ListItem::new(x.language.name().to_owned()).fg(self.theme.fg));
-        let list_widget = List::new(list_items).highlight_style(if self.focus == FOCUS_LIST {
+        let list_widget = List::new(list_items).highlight_style(if self.focus == Focus::List {
             Style::default()
                 .fg(self.theme.selected_fg)
                 .bg(self.theme.selected_bg)
